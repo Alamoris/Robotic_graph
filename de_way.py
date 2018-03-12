64ef9ced82c0d3f6bf1ba9795b67167688051c3a
Volga Bogoslovsky <gonzalez1139@gmail.com>
2018-03-12 22:32:06 +0300

    Add function to know de way

--- de_way.py
+++ de_way.py
@@ -24,67 +24,134 @@ def fill_map(x_coords, y_coords):
             pixels[(40 * x_coords) + x, (40 * y_coords) + i] = (0, 0, 0)
 
 
-def markup_func(start_point, finish_point):
-    def step(cell, step_num, usable_array):
-        cell_x = cell[0]
-        cell_y = cell[1]
+def expansion_algorithm(start_point, finish_point):
+    def markup_func(start, finish):
+        def step(cell, step_num, usable_array):
+            cell_x = cell[0]
+            cell_y = cell[1]
+
+            next_cell = MAP_ARRAY[cell_x - 1][cell_y]
+            if cell_x - 1 > -1:
+                if next_cell == 0:
+                    MAP_ARRAY[cell_x - 1][cell[1]] = step_num + 1
+                    usable_array.append((cell_x - 1, cell_y))
+
+            if cell_y - 1 > -1:
+                next_cell = MAP_ARRAY[cell_x][cell_y - 1]
+                if next_cell == 0:
+                    MAP_ARRAY[cell_x][cell_y - 1] = step_num + 1
+                    usable_array.append((cell_x, cell_y - 1))
+
+            if cell_x + 1 < map_size:
+                next_cell = MAP_ARRAY[cell_x + 1][cell_y]
+                if next_cell == 0:
+                    MAP_ARRAY[cell_x + 1][cell_y] = step_num + 1
+                    usable_array.append((cell_x + 1, cell_y))
+
+            if cell_y + 1 < map_size:
+                next_cell = MAP_ARRAY[cell_x][cell_y + 1]
+                if next_cell == 0:
+                    MAP_ARRAY[cell_x][cell_y + 1] = step_num + 1
+                    usable_array.append((cell_x, cell_y + 1))
+
+        first_wave_array = []
+        second_wave_array = []
+        step_value = 0
         map_size = len(MAP_ARRAY)
-        print(cell_x)
-        print(cell_y)
 
-        next_cell = MAP_ARRAY[cell_x - 1][cell_y]
-        if cell_x - 1 != -1:
-            if next_cell == 0:
-                MAP_ARRAY[cell_x - 1][cell[1]] = step_num + 1
-                usable_array.append((cell_x - 1, cell_y))
+        # Initialise first step
+        MAP_ARRAY[start[0]][start[1]] = -9
+        step(start, step_value, first_wave_array)
+        print(f"first array = {first_wave_array}, \n second array = {second_wave_array}")
+        while True:
+            step_value += 1
 
-        if cell_y - 1 != -1:
-            next_cell = MAP_ARRAY[cell_x][cell_y - 1]
-            if next_cell == 0:
-                MAP_ARRAY[cell_x][cell_y - 1] = step_num + 1
-                usable_array.append((cell_x, cell_y - 1))
+            if (step_value % 2) == 1:
+                current_array = first_wave_array
+                custody_array = second_wave_array
+            else:
+                current_array = second_wave_array
+                custody_array = first_wave_array
 
-        if cell_x + 1 < map_size:
-            next_cell = MAP_ARRAY[cell_x + 1][cell_y]
-            if next_cell == 0:
-                MAP_ARRAY[cell_x + 1][cell_y] = step_num + 1
-                usable_array.append((cell_x + 1, cell_y))
+            print(current_array)
+            for x in current_array:
+                step(x, step_value, custody_array)
+            current_array.clear()
 
-        if cell_y + 1 < map_size:
-            next_cell = MAP_ARRAY[cell_x][cell_y + 1]
-            if next_cell == 0:
-                MAP_ARRAY[cell_x][cell_y + 1] = step_num + 1
-                usable_array.append((cell_x, cell_y + 1))
-
-
-    first_wave_array = []
-    second_wave_array = []
-    step_value = 0
-
-    # Initialise first step
-    MAP_ARRAY[start_point[0]][start_point[1]] = -9
-    step(start_point, step_value, first_wave_array)
-    print(f"first array = {first_wave_array}, \n second array = {second_wave_array}")
-    while True:
-
+            if MAP_ARRAY[finish[0]][finish[1]] != 0:
+                print(f"first array = {first_wave_array}, \n second array = {second_wave_array}")
+                break
         step_value += 1
+        return step_value
 
-        if (step_value % 2) == 1:
-            current_array = first_wave_array
-            custody_array = second_wave_array
-        else:
-            current_array = second_wave_array
-            custody_array = first_wave_array
+    def find_de_way(start_point, finish_point, step_val):  # clack clack clack
+        # Searching next station of way
+        def search_step(cell, step_num, start, finish):
+            cell_x = cell[0]
+            cell_y = cell[1]
+            print(cell_x)
+            print(cell_y)
 
-        print(current_array)
-        for x in current_array:
-            step(x, step_value, custody_array)
-        current_array.clear()
+            if cell_x - 1 != -1:
+                next_cell = MAP_ARRAY[cell_x - 1][cell_y]
+                if next_cell == step_num - 1:
+                    de_way.append((cell_x - 1, cell_y))
+                    step_num -= 1
+                    return (cell_x - 1, cell_y), step_num
+                elif next_cell == -9:
+                    de_way.append((cell_x - 1, cell_y))
+                    return (cell_x - 1, cell_y), step_num
 
-        if MAP_ARRAY[finish_point[0]][finish_point[1]] != 0:
-            print(f"first array = {first_wave_array}, \n second array = {second_wave_array}")
-            break
+            if cell_y - 1 != -1:
+                next_cell = MAP_ARRAY[cell_x][cell_y - 1]
+                if next_cell == step_num - 1:
+                    de_way.append((cell_x, cell_y - 1))
+                    step_num -= 1
+                    return (cell_x, cell_y - 1), step_num
+                elif next_cell == -9:
+                    de_way.append((cell_x, cell_y - 1))
+                    return (cell_x, cell_y - 1), step_num
 
+            if cell_x + 1 < map_size:
+                next_cell = MAP_ARRAY[cell_x + 1][cell_y]
+                if next_cell == step_num - 1:
+                    de_way.append((cell_x + 1, cell_y))
+                    step_num -= 1
+                    return (cell_x + 1, cell_y), step_num
+                elif next_cell == -9:
+                    de_way.append((cell_x + 1, cell_y))
+                    return (cell_x + 1, cell_y), step_num
+
+            if cell_y + 1 < map_size:
+                next_cell = MAP_ARRAY[cell_x][cell_y + 1]
+                if next_cell == step_num - 1:
+                    de_way.append((cell_x, cell_y + 1))
+                    step_num -= 1
+                    return (cell_x, cell_y + 1), step_num
+                elif next_cell == -9:
+                    de_way.append((cell_x, cell_y + 1))
+                    return (cell_x, cell_y + 1), step_num
+
+        de_way = []
+        step_value = step_val
+        current_cell = finish_point
+        map_size = len(MAP_ARRAY)
+        i = 0
+        while True:
+            i += 1
+            print(i)
+
+            work_cell, value = search_step(current_cell, step_value, start_point, finish_point)
+            current_cell = work_cell
+            step_value = value
+
+            if current_cell == start_point:
+                return de_way
+
+    de_way_len = markup_func(start_point, finish_point)
+    print(de_way_len)
+    way = find_de_way(start_point, finish_point, de_way_len)
+    print(way)
 
 for x in OBSTACLES:
     MAP_ARRAY[x[0], x[1]] = -1
@@ -115,7 +182,7 @@ for x in range(8):
         if graph.check_empty(x * 8 + (i + 1)):
             fill_map(i, x)
 
-markup_func((7, 0), (7, 7))
+expansion_algorithm((7, 0), (7, 7))
 
 for x in MAP_ARRAY:
     print(x)
